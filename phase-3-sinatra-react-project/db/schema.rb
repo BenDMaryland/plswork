78e790e821f3864c603e230fde0e59cef9951efc
BenDMaryland <ben.d.maryland@gmail.com>
2021-11-18 17:07:33 -0500

    pls

--- phase-3-sinatra-react-project/db/schema.rb
+++ phase-3-sinatra-react-project/db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_11_15_234244) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "game_instances", force: :cascade do |t|
     t.integer "winner"
     t.integer "loser"
